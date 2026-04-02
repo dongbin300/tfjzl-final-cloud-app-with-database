7c0a50b2a0c7d51a9fb632fc65581cbb08ad079e
Gaten <dongbin300@gmail.com>
2026-04-02 15:33:31 +0900

    Build New Models: Question, Choice, Submission

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -95,9 +95,41 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+class Question(models.Model):
+    # Foreign key to course
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    # Question text
+    content = models.CharField(max_length=200)
+    # Question grade point
+    grade = models.IntegerField(default=50)
+
+    def __str__(self):
+        return "Question: " + self.content
+
+    # Method to calculate if the learner gets the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+        
+
+class Choice(models.Model):
+    # Foreign key to question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # Choice content as text
+    content = models.CharField(max_length=200)
+    # Is choice correct as boolean
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.content
+    
+    
+class Submission(models.Model):
+    # Many-to-one relationship with Enrollment
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    # Many-to-many relationship with Choice
+    choices = models.ManyToManyField(Choice)
